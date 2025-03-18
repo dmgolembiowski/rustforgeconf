f0ebffe71ddd61fbb09a098d81463310de862ea3
Tim McNamara <tim@accelerant.dev>
2025-03-19 00:36:48 +1300

    Happier with powerline

--- beta.rustforgeconf.com/src/partial/Header_Nav.php
+++ beta.rustforgeconf.com/src/partial/Header_Nav.php
@@ -5,23 +5,60 @@ namespace rfc\ws\partial;
 class Header_Nav
 {
     const NAV_ITEMS = [
-        'speak'    => ["/speak", "Speak"],
-        'attend'   => ["/attend", "Attend"],
-        'sponsor'  => ["/sponsor", "Sponsor"],
+        'speak'    => ["/speak", "Speak", []],
+        'attend'   => ["/attend", "Attend", []],
+        'sponsor'  => ["/sponsor", "Sponsor", []],
         'events'   => ["/events", "Events", [
-            'social'      => ["/social", "Social"],
-            'team-spaces' => ["/team-spaces", "Team Spaces"],
-            'workshop'    => ["/how-to-adopt-rust-workshop", "Workshop"],
-            'talks'       => ["/talks", "Talks"]
+            'social'      => ["/social", "Social", []],
+            'team-spaces' => ["/team-spaces", "Team Spaces", []],
+            'workshop'    => ["/how-to-adopt-rust-workshop", "Workshop", []],
+            'talks'       => ["/talks", "Talks", []],
         ]],
         'location' => ["/location", "Location", [
-            'venue'         => ["/venue", "Venue"],
-            'travel'        => ["/travel", "Travel"],
-            'accommodation' => ["/accommodation", "Accommodation"],
+            'venue'         => ["/venue", "Venue", []],
+            'travel'        => ["/travel", "Travel", [
+                'travel/intl' => ["/travel/intl", "Recommended Timetable", []],
+                'travel/visa' => ["/travel/visa", "Visa Requirements", []]],
+            ],
+            'accommodation' => ["/accommodation", "Accommodation", []],
         ]],
-        'news'     => ["/news", "News"],
-        'about'    => ["/about", "About"]
+        'news'     => ["/news", "News", []],
+        'about'    => ["/about", "About", []],
     ];
+
+    const BREADCRUMBS = [
+        '/'                           => [['/', '~']],
+        '/attend'                     => [['/', '~'], ['/attend', 'Attend']],
+        '/speak'                      => [['/', '~'], ['/speak', 'Speak']],
+        '/sponsor'                    => [['/', '~'], ['/sponsor', 'Sponsor']],
+        '/events'                     => [['/', '~'], ['/events', 'Events']],
+        '/social'                     => [['/', '~'], ['/events', 'Events'], ['/social', 'Social']],
+        '/team-spaces'                => [['/', '~'], ['/events', 'Events'], ['/team-spaces', 'Team Spaces']],
+        '/workshop'                   => [['/', '~'], ['/events', 'Events'], ['/how-to-adopt-rust-workshop', 'Workshop']],
+        '/how-to-adopt-rust-workshop' => [['/', '~'], ['/events', 'Events'], ['/how-to-adopt-rust-workshop', 'Workshop']],
+        '/talks'                      => [['/', '~'], ['/events', 'Events'], ['/talks', 'Talks']],
+        '/location'                   => [['/', '~'], ['/location', 'Location']],
+        '/venue'                      => [['/', '~'], ['/location', 'Location'], ['/venue', 'Venue']],
+        '/travel'                     => [['/', '~'], ['/location', 'Location'], ['/travel', 'Travel']],
+        '/travel/intl'                => [['/', '~'], ['/location', 'Location'], ['/travel', 'Travel'], ['/travel/intl', 'Recommended Timetable']],
+        '/travel/visa'                => [['/', '~'], ['/location', 'Location'], ['/travel', 'Travel'], ['/travel/visa', 'Visa Requirements']],
+        '/accommodation'              => [['/', '~'], ['/location', 'Location'], ['/accommodation', 'Accommodation']],
+        '/news'                       => [['/', '~'], ['/news', 'News']],
+        '/about'                      => [['/', '~'], ['/about', 'About']],
+    ];
+
+    public static function breadcrumbs(string $uri): array
+    {
+        // TODO: create this array dynamically, rather than pre-computing a table
+
+        // all articles should point to the news
+        if (!empty(stristr($uri, '/news'))) {
+            $uri = '/news';
+        }
+
+        $crumbs = self::BREADCRUMBS[$uri] ?? [['/', '~']];
+        return $crumbs;
+    }
 
     public static function parent_item(string $uri, string $active_item): string
     {
@@ -35,6 +72,30 @@ class Header_Nav
             }
         }
         return $active_item;
+    }
+
+    public static function child_items(string $uri): array
+    {
+        // breadth-first search, e.g. try the top-level headings first
+        $queue = new \SplQueue();
+        
+        foreach (self::NAV_ITEMS as $key => $item) {
+            $queue->enqueue([$key, $item]);
+        }
+        
+        while (!$queue->isEmpty()) {
+            [$_item_name, $item_details] = $queue->dequeue();
+            
+            if ($item_details[0] === $uri) {
+                return $item_details[2];
+            }
+            
+            foreach ($item_details[2] as $next_item_name =>$next_item_details) {
+                $queue->enqueue([$next_item_name, $next_item_details]);
+            }
+        }
+        
+        return [];
     }
 
     public static function active_item(string $uri): string
@@ -71,49 +132,31 @@ class Header_Nav
         $uri         = $_SERVER['REQUEST_URI'];
         $active_item = self::active_item($uri);
         $parent_item = self::parent_item($uri, $active_item);
-        $child_nav_items = self::NAV_ITEMS[$parent_item][2] ?? [];
+        $child_items = self::child_items($uri);
 
-        echo '<div>';
-        echo '<nav class="horizontal-nav">';
+        $crumbs = self::breadcrumbs($uri);
 
-        self::render_link(
-            'home',
-            '/',
-            '~',
-            self::classes('', $active_item, $parent_item)
-        );
+        echo '<div id="powerline">';
+        echo '<nav class="horizontal-nav" >';
 
-        if ( !empty($child_nav_items) && !empty( $parent_item )  ) {
-            $p = self::NAV_ITEMS[$parent_item];
-            self::render_link(
-                $parent_item,
-                $p[0],
-                $p[1],
-                self::classes($parent_item, $active_item, $parent_item)
-            );
-          
-        echo '<ul>';
-        self::render_nav_items(self::NAV_ITEMS, $uri, $active_item, $parent_item);
-        echo '</ul></nav>';
+        foreach ($crumbs as $crumb) {
+            self::render_link('', $crumb[0], $crumb[1], 'ancestor');
+        }
 
-
-        $child_nav_items = self::NAV_ITEMS[$parent_item][2] ?? [];
-
-        if (!empty($child_nav_items)) {
+        if (!empty($child_items)) {
             echo '<nav class="horizontal-nav sub-nav">';
             echo '<ul>';
-            self::render_nav_items($child_nav_items, $uri, $active_item, $parent_item);
+            self::render_nav_items($child_items, $uri, $active_item, $parent_item);
             echo '</ul>';
             echo '</nav>';
-        } else {
-
+        } else if ($uri === '/') {
+            echo '<nav class="horizontal-nav sub-nav">';
             echo '<ul>';
             self::render_nav_items(self::NAV_ITEMS, $uri, $active_item, $parent_item);
             echo '</ul></nav>';
         }
 
-        echo '</div>';
-
+        echo '</div>'; // powerline
     }
 
     public static function render_nav_items(
